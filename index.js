34710a06d7bf3078bfb59f61ba12fefef64506f3
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-15 12:43:40 +0200

    Added openweathermap API

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
+const fetch = require('node-fetch');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const key = '';
+
+let city = 'Tartu';
+
 app.get('/', (req, res) => {
-    res.render('index');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(res => res.json())
+        .then(data => {
+            let city = data.name;
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15);
+            let desc = data.weather[0].description;
+
+            res.render('index', {
+                city: city,
+                temp: temp,
+                desc: desc
+            });
+        })
 })
 
 app.listen(3001);
